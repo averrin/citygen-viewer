4cbc3fc72889bc5151f545924c7ba14f1cb3e08f
averrin <averrin@gmail.com>
2018-10-21 12:26:50 +0300

    init view

--- include/app/application.hpp
+++ include/app/application.hpp
@@ -1,25 +1,29 @@
 #ifndef __APPLICATION_H_
 #define __APPLICATION_H_
-#include <string>
-#include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <string>
 
 #include <liblog/liblog.hpp>
 
 class Application {
 public:
-        Application(std::string, std::string);
-        ~Application() = default;
+  Application(std::string, std::string);
+  ~Application() = default;
 
-        std::string APP_NAME;
-        std::string VERSION;
+  std::string APP_NAME;
+  std::string VERSION;
 
-        sf::RenderWindow *window;
-        void processEvent(sf::Event event);
-        int serve();
-        void drawMainWindow();
-        Logger& log = Logger::getInstance();
+  sf::RenderWindow *window;
+  void processEvent(sf::Event event);
+  int serve();
+  void drawMainWindow();
+  Logger &log = Logger::getInstance();
+
+  sf::View minimap;
+  sf::View fixed;
+  sf::RectangleShape miniback;
+  float zoom = 10.f;
 };
-
 
 #endif // __APPLICATION_H_
--- src/application.cpp
+++ src/application.cpp
@@ -13,10 +13,37 @@ Application::Application(std::string app_name, std::string version)
 
   ImGuiIO &io = ImGui::GetIO();
 
-  window = new sf::RenderWindow(sf::VideoMode::getDesktopMode(), APP_NAME,
+  // window = new sf::RenderWindow(sf::VideoMode::getDesktopMode(), APP_NAME,
+  window = new sf::RenderWindow(sf::VideoMode(1600, 900), APP_NAME,
                                 sf::Style::Default, settings);
   window->setVerticalSyncEnabled(true);
+  window->setFramerateLimit(60);
   ImGui::SFML::Init(*window);
+
+  fixed = window->getView(); // The 'fixed' view will never change
+  sf::View standard =
+      fixed; // The 'standard' view will be the one that gets always displayed
+
+  unsigned int size =
+      400; // The 'minimap' view will show a smaller picture of the map
+  minimap = sf::View(sf::FloatRect(
+      standard.getCenter().x, standard.getCenter().y, static_cast<float>(size),
+      static_cast<float>(window->getSize().y * size / window->getSize().x)));
+  minimap.setViewport(sf::FloatRect(
+      1.f - static_cast<float>(minimap.getSize().x) / window->getSize().x -
+          0.02f,
+      1.f - static_cast<float>(minimap.getSize().y) / window->getSize().y -
+          0.02f,
+      static_cast<float>(minimap.getSize().x) / window->getSize().x,
+      static_cast<float>(minimap.getSize().y) / window->getSize().y));
+  minimap.zoom(zoom);
+
+  miniback.setPosition(minimap.getViewport().left * window->getSize().x - 5,
+                       minimap.getViewport().top * window->getSize().y - 5);
+  miniback.setSize(
+      sf::Vector2f(minimap.getViewport().width * window->getSize().x + 10,
+                   minimap.getViewport().height * window->getSize().y + 10));
+  miniback.setFillColor(sf::Color(160, 8, 8));
 
   window->resetGLStates();
 }
@@ -29,6 +56,18 @@ void Application::processEvent(sf::Event event) {
     case sf::Keyboard::Escape:
       window->close();
       break;
+    case sf::Keyboard::Right:
+      fixed.move(20.f, 0.f);
+      break;
+    case sf::Keyboard::Left:
+      fixed.move(-20.f, 0.f);
+      break;
+    case sf::Keyboard::Up:
+      fixed.move(0.f, -20.f);
+      break;
+    case sf::Keyboard::Down:
+      fixed.move(0.f, 20.f);
+      break;
     }
     break;
   case sf::Event::Closed:
@@ -38,16 +77,58 @@ void Application::processEvent(sf::Event event) {
 }
 
 float value = 1.f;
-int count = 15;
-float size = 50.f;
+int count = 25;
+float size = 100.f;
 
 void Application::drawMainWindow() {
   ImGui::Begin(APP_NAME.c_str());
-  ImGui::Text("\n\nSFML + ImGui starter (%s)\n\n", VERSION.c_str());
   ImGui::SliderInt("Count", &count, 1, 200);
   ImGui::SliderFloat("Size", &size, 4.f, 200.f);
   ImGui::SliderFloat("Value", &value, 0.f, 1.f);
+
   ImGui::End();
+}
+
+std::shared_ptr<sf::RenderTexture> drawMap() {
+  auto tex = std::make_shared<sf::RenderTexture>();
+  tex->create(2400, 1800);
+
+  auto bgColor = sf::Color(23, 23, 23);
+  tex->clear(bgColor);
+
+  float padding = size / 10;
+  auto yOffset = 0.f;
+  for (auto y = 0; y < count; y++) {
+    auto offset = 0.f;
+    auto startColor = Color::fromWebName("red");
+    startColor.saturation(1 - y / float(count));
+    startColor.value(value);
+    for (auto i = 0; i < count; i++) {
+      auto sfColor =
+          sf::Color(startColor.red(), startColor.green(), startColor.blue());
+
+      sf::VertexArray quad(sf::Quads, 4);
+
+      quad[0].position = sf::Vector2f(padding + offset, padding + yOffset);
+      quad[0].color = sfColor;
+      quad[1].position =
+          sf::Vector2f(padding + size + offset, padding + yOffset);
+      quad[1].color = sfColor;
+      quad[2].position =
+          sf::Vector2f(padding + size + offset, padding + size + yOffset);
+      quad[2].color = sfColor;
+      quad[3].position =
+          sf::Vector2f(padding + offset, padding + size + yOffset);
+      quad[3].color = sfColor;
+
+      tex->draw(quad);
+      offset += size + padding;
+      startColor.hue(startColor.hue() + 360.f / count);
+    }
+    yOffset += size + padding;
+  }
+
+  return tex;
 }
 
 int Application::serve() {
@@ -58,37 +139,30 @@ int Application::serve() {
     while (window->pollEvent(event)) {
       processEvent(event);
     }
+
+    window->setView(fixed);
     auto bgColor = sf::Color(23, 23, 23);
     window->clear(bgColor);
 
-    float padding = size/10;
-    auto yOffset = 0.f;
-    for (auto y = 0; y < count; y++) {
-      auto offset = 0.f;
-      auto startColor = Color::fromWebName("red");
-      startColor.saturation(1 - y/float(count));
-      startColor.value(value);
-      for (auto i = 0; i < count; i++) {
-        auto sfColor =
-            sf::Color(startColor.red(), startColor.green(), startColor.blue());
+    auto texture = drawMap();
+    texture->display();
+    sf::Sprite sprite(texture->getTexture());
+    window->draw(sprite);
 
-        sf::VertexArray quad(sf::Quads, 4);
-
-        quad[0].position = sf::Vector2f(padding + offset, padding + yOffset);
-        quad[0].color = sfColor;
-        quad[1].position = sf::Vector2f(padding + size + offset, padding + yOffset);
-        quad[1].color = sfColor;
-        quad[2].position = sf::Vector2f(padding + size + offset, padding + size + yOffset);
-        quad[2].color = sfColor;
-        quad[3].position = sf::Vector2f(padding + offset, padding + size + yOffset);
-        quad[3].color = sfColor;
-
-        window->draw(quad);
-        offset += size + padding;
-        startColor.hue(startColor.hue() + 360.f / count);
-      }
-      yOffset += size + padding;
-    }
+    sf::RectangleShape frame;
+    int t = 20;
+    frame.setPosition(fixed.getCenter().x - window->getSize().x / 2,
+                      fixed.getCenter().y - window->getSize().y / 2);
+    frame.setSize(
+        sf::Vector2f(fixed.getViewport().width * window->getSize().x + t,
+                     fixed.getViewport().height * window->getSize().y + t));
+    frame.setFillColor(sf::Color::Transparent);
+    frame.setOutlineThickness(t);
+    frame.setOutlineColor(sf::Color(200, 200, 200));
+    texture->draw(frame);
+    texture->display();
+    window->setView(minimap); // Draw minimap
+    window->draw(sprite);
 
     ImGui::SFML::Update(*window, deltaClock.restart());
     drawMainWindow();
